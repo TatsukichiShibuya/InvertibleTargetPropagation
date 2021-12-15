ff348d15b56887a02d1ced5a62dc5050ad7f2d4d
TatsukichiShibuya <tatsukichi78@gmail.com>
2021-12-15 12:53:01 +0900

    udpate

--- code/job_MyTP.sh
+++ code/job_MyTP.sh
@@ -4,4 +4,9 @@
 #$ -l h_rt=12:00:00
 #$ -j y
 #$ -o output/o.$JOB_ID
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 wandb agent tatsukichishibuya/InvertibleTargetPropagation/xeb4wgjp
