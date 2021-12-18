20ca36cafd4a8e7b656eb3d27ef9c3b3ece2afa9
TatsukichiShibuya <tatsukichi78@gmail.com>
2021-12-18 21:44:50 +0900

    udpate

--- code/job_DTTP.sh
+++ code/job_DTTP.sh
@@ -9,4 +9,4 @@ export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
-wandb agent tatsukichishibuya/InvertibleTargetPropagation/mp9h5spw
+wandb agent tatsukichishibuya/InvertibleTargetPropagation/f8zkxel7
