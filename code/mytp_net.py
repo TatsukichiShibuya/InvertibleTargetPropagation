1a814dfd8f2bddf28612de74eb35d3ba13802999
TatsukichiShibuya <tatsukichi78@gmail.com>
2021-12-20 21:36:21 +0900

    update

--- code/mytp_net.py
+++ code/mytp_net.py
@@ -79,14 +79,14 @@ class mytp_net(net):
                 t_ = self.layers[self.depth - self.direct_depth].target
                 v1, v2, v3 = t - h, t_ - h, t - t_
                 target_angle.append(calc_angle(v1, v2).mean())
-                target_dist.append((torch.norm(v3, dim=1) / (torch.norm(v2, dim=1) + 1e-12)).mean())
+                target_dist.append((torch.norm(v3, dim=1) / (torch.norm(v2, dim=1) + 1e-30)).mean())
 
-                eig1, _ = torch.linalg.eig(self.layers[1].weight @ self.layers[1].backweight -
+                """eig1, _ = torch.linalg.eig(self.layers[1].weight @ self.layers[1].backweight -
                                            torch.eye(self.layers[1].weight.shape[0], device=self.device))
                 eig2, _ = torch.linalg.eig(self.layers[2].weight @ self.layers[2].backweight -
                                            torch.eye(self.layers[2].weight.shape[0], device=self.device))
                 print(eig1.real.max())
-                print(eig2.real.max())
+                print(eig2.real.max())"""
 
                 monitor_end_time = time.time()
                 monitor_time += monitor_end_time - monitor_start_time
@@ -213,10 +213,7 @@ class mytp_net(net):
                         delta = self.layers[d + 1].target - fgt
                         u = u + delta
                     self.layers[d].target = self.layers[d + 1].backward(u)
-                    print(f"delta {d}", torch.norm(delta, dim=1).max())
-                    if torch.norm(delta, dim=1).max() > 0.1:
-                        import pdb
-                        pdb.set_trace()
+                    # print(f"delta {d}", torch.norm(delta, dim=1).max())
 
     def update_weights(self, x, lr_ratio, scaling=False):
         self.forward(x)
@@ -226,7 +223,7 @@ class mytp_net(net):
         for d in reversed(range(self.depth)):
             # compute grad
             local_loss = ((self.layers[d].target - self.layers[d].linear_activation)**2).sum(axis=1)
-            lr = (global_loss / (local_loss + 1e-12)).reshape(-1, 1) if d < D else torch.tensor(1)
+            lr = (global_loss / (local_loss + 1e-30)).reshape(-1, 1) if d < D else torch.tensor(1)
             n = self.layers[d].activation / \
                 (self.layers[d].activation**2).sum(axis=1).reshape(-1, 1)
             grad = (self.layers[d].target - self.layers[d].linear_activation).T @ (n * lr**lr_ratio)
