ef6a94defc1e211f479534df4104534918dc7bd9
TatsukichiShibuya <tatsukichi78@gmail.com>
2021-12-20 17:24:06 +0900

    update

--- code/mytp_net.py
+++ code/mytp_net.py
@@ -202,11 +202,13 @@ class mytp_net(net):
             elif refinement_type == "fg":
                 for d in reversed(range(self.depth - self.direct_depth)):
                     u = self.layers[d + 1].target
+                    print(f"target {d}", torch.norm(u, dim=1)[0])
                     for i in range(refinement_iter):
                         gt = self.layers[d + 1].backward(u)
                         fgt = self.layers[d + 1].forward(gt, update=False)
                         delta = self.layers[d + 1].target - fgt
                         u = u + delta
+                    print(f"target {d}", torch.norm(u, dim=1)[0])
                     print(f"delta {d}", torch.norm(delta, dim=1).max())
                     self.layers[d].target = self.layers[d + 1].backward(u)
                 """for i in range(refinement_iter):
