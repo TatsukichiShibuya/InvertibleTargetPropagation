724c4795100377112d17f8ec2367f88c44e71eda
TatsukichiShibuya <tatsukichi78@gmail.com>
2021-12-30 18:39:28 +0900

    update

--- code/mytp_net.py
+++ code/mytp_net.py
@@ -245,6 +245,7 @@ class mytp_net(net):
             # compute grad
             local_loss = ((self.layers[d].target - self.layers[d].linear_activation)**2).sum(axis=1)
             lr = (global_loss / (local_loss + 1e-30)).reshape(-1, 1)
+            lr = lr / len(self.layeres[d].target)
             n = self.layers[d].activation / \
                 (self.layers[d].activation**2).sum(axis=1).reshape(-1, 1)
             grad = (self.layers[d].target - self.layers[d].linear_activation).T @ (n * lr**lr_ratio)
