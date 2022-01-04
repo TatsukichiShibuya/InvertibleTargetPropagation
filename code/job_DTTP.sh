3cbfadcd4b98fff4e0828cf0fc616b8e26fbefb7
TatsukichiShibuya <tatsukichi78@gmail.com>
2022-01-04 17:49:43 +0900

    update

--- code/job_DTTP.sh
+++ code/job_DTTP.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$ -cwd
 #$ -l f_node=1
-#$ -l h_rt=23:55:00
+#$ -l h_rt=04:00:00
 #$ -j y
 #$ -o output/o.$JOB_ID
 export PYENV_ROOT="$HOME/.pyenv"
@@ -9,8 +9,20 @@ export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
-if [ "${1}" = "lr_ratio" ];then
-  wandb agent tatsukichishibuya/InvertibleTargetPropagation/t49y28lk
-else
-  wandb agent tatsukichishibuya/InvertibleTargetPropagation/m48rv2it
+if [ "${1}" = "CCC" ];then
+  wandb agent tatsukichishibuya/InvertibleTargetPropagation/laf3350d
+elif [ "${1}" = "CCT" ];then
+  wandb agent tatsukichishibuya/InvertibleTargetPropagation/08kjgdq0
+elif [ "${1}" = "CTC" ];then
+  wandb agent tatsukichishibuya/InvertibleTargetPropagation/n5tmufzl
+elif [ "${1}" = "CTT" ];then
+  wandb agent tatsukichishibuya/InvertibleTargetPropagation/4i3pgy1a
+elif [ "${1}" = "TCC" ];then
+  wandb agent tatsukichishibuya/InvertibleTargetPropagation/y0gmadlu
+elif [ "${1}" = "TCT" ];then
+  wandb agent tatsukichishibuya/InvertibleTargetPropagation/e4fqv2jk
+elif [ "${1}" = "TTC" ];then
+  wandb agent tatsukichishibuya/InvertibleTargetPropagation/w41a55s3
+elif [ "${1}" = "TTT" ];then
+  wandb agent tatsukichishibuya/InvertibleTargetPropagation/hy2tkarj
 fi
