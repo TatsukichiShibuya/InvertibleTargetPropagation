59458ffcde0de62b110b09676a82482cf6d73f88
TatsukichiShibuya <tatsukichi78@gmail.com>
2022-01-07 21:37:46 +0900

    update

--- code/dttp_net.py
+++ code/dttp_net.py
@@ -67,12 +67,10 @@ class dttp_net(net):
             torch.cuda.empty_cache()
             # monitor
             refinement_converge = [[] for d in range(self.depth - self.direct_depth)]
-            """
             target_ratio_list = [torch.tensor([], device=self.device)
                                  for d in range(self.depth - self.direct_depth)]
             target_angle_list = [torch.tensor([], device=self.device)
                                  for d in range(self.depth - self.direct_depth)]
-            """
             monitor_time = 0
             start_time = time.time()
 
@@ -93,7 +91,7 @@ class dttp_net(net):
                 ret = self.check_refinement()
                 for d in range(self.depth - self.direct_depth):
                     refinement_converge[d].append(ret[d])
-                """
+
                 D = self.depth - self.direct_depth
                 for d1 in range(D):
                     t = self.layers[d1].target
@@ -105,9 +103,9 @@ class dttp_net(net):
                     target_ratio_list[d1] = torch.cat([target_ratio_list[d1], target_ratio])
                     target_angle = calc_angle(v1, v2)
                     target_angle_list[d1] = torch.cat([target_angle_list[d1], target_angle])
-                    print(f"ratio : < {target_ratio.max()}")
-                    print(f"angle : < {target_angle.max()}")
-                """
+                    #print(f"ratio : < {target_ratio.max()}")
+                    #print(f"angle : < {target_angle.max()}")
+
                 monitor_end_time = time.time()
                 monitor_time = monitor_time + monitor_end_time - monitor_start_time
                 ###### monitor end ######
@@ -142,10 +140,8 @@ class dttp_net(net):
                     for d in range(self.depth - self.direct_depth):
                         x = torch.tensor(refinement_converge[d])
                         log_dict[f"convergence {d}"] = (torch.sum(x) / len(x)).item()
-                        """
                         log_dict[f"target ratio {d}"] = torch.mean(target_ratio_list[d]).item()
                         log_dict[f"target angle {d}"] = torch.mean(target_angle_list[d]).item()
-                        """
 
                     wandb.log(log_dict)
                 else:
@@ -162,10 +158,8 @@ class dttp_net(net):
                     for d in range(self.depth - self.direct_depth):
                         x = torch.tensor(refinement_converge[d])
                         print(f"\tconvergence {d}: {(torch.sum(x) / len(x)).item()}")
-                        """
                         print(f"\ttarget ratio {d}: {torch.mean(target_ratio_list[d]).item()}")
                         print(f"\ttarget angle {d}: {torch.mean(target_angle_list[d]).item()}")
-                        """
 
     def check_refinement(self):
         ret = []
@@ -298,7 +292,7 @@ class dttp_net(net):
             n = self.layers[d].activation / \
                 (self.layers[d].activation**2).sum(axis=1).reshape(-1, 1)
             grad = (self.layers[d].target -
-                    self.layers[d].linear_activation).T @ (n * (lr / batch_size))
+                    self.layers[d].linear_activation).T @ (n * (lr**lr_ratio / batch_size))
 
             # update weight
             if not (torch.isnan(grad).any() or torch.isinf(grad).any()
