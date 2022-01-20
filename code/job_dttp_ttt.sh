2362f417276f13188ce48991d9889e7217af665d
TatsukichiShibuya <tatsukichi78@gmail.com>
2022-01-20 16:18:38 +0900

    update

--- code/job_dttp_ttt.sh
+++ code/job_dttp_ttt.sh
@@ -9,4 +9,4 @@ export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
-wandb agent tatsukichishibuya/InvertibleTargetPropagation/thwbnla4
+wandb agent tatsukichishibuya/InvertibleTargetPropagation/2mn3vjme
