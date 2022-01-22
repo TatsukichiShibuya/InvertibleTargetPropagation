c80b02537844f1537411b945eb2a7f3b978eda37
TatsukichiShibuya <tatsukichi78@gmail.com>
2022-01-23 00:49:21 +0900

    update

--- code/job_invtp.sh
+++ code/job_invtp.sh
@@ -10,5 +10,5 @@ eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 if [ "${1}" = "invCC" ];then
-  wandb agent tatsukichishibuya/InvertibleTargetPropagation/1bq1g631
+  wandb agent tatsukichishibuya/InvertibleTargetPropagation/ezbberyp
 fi
