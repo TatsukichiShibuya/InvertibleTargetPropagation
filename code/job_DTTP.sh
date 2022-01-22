92855769242025207bad49cb5b090ee3721cb78b
TatsukichiShibuya <tatsukichi78@gmail.com>
2022-01-22 16:04:12 +0900

    update

--- code/job_DTTP.sh
+++ code/job_DTTP.sh
@@ -9,8 +9,8 @@ export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
-if [ "${1}" = "CCC1" ];then
-  wandb agent tatsukichishibuya/InvertibleTargetPropagation/bz1oikpi
-elif [ "${1}" = "CCC2" ];then
-  wandb agent tatsukichishibuya/InvertibleTargetPropagation/fjw4745o
+if [ "${1}" = "TTT5" ];then
+  wandb agent tatsukichishibuya/InvertibleTargetPropagation/13uqr8t7
+elif [ "${1}" = "TTT7" ];then
+  wandb agent tatsukichishibuya/InvertibleTargetPropagation/r9fyv32v
 fi
