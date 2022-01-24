352c9befe25cb6a669d78a7704687e7d2e87db76
TatsukichiShibuya <tatsukichi78@gmail.com>
2022-01-24 20:44:30 +0900

    update

--- code/job_DTTP.sh
+++ code/job_DTTP.sh
@@ -9,5 +9,4 @@ export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
-python Main.py --algorithm=DTTP --activation_function=tanh --b_epochs=3 --b_sigma=0.04299251373155531 --epochs=200 --learning_rate=2.7985185386558067 --learning_rate_for_backward=0.02874860776145025 --refinement_iter=50 --stepsize=0.02 --type=TTT --log --agent
 python Main.py --algorithm=DTTP --activation_function=tanh --b_epochs=3 --b_sigma=0.04684015427761784 --depth=4 --epochs=200 --hid_dim=1024 --in_dim=3072 --learning_rate=0.17762986867591446 --learning_rate_for_backward=0.003205928432704797 --problem=CIFAR10 --stepsize=0.02 --type=CCC --log --agent
