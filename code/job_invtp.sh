f5a84f90eda9d518bd40df72049f4e742ad978be
TatsukichiShibuya <tatsukichi78@gmail.com>
2022-02-27 02:01:19 +0900

    update

--- code/job_invtp.sh
+++ code/job_invtp.sh
@@ -9,4 +9,4 @@ export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
-wandb agent tatsukichishibuya/InvTP/zzml8i5q
+wandb agent tatsukichishibuya/InvTP/a9j7d2ku
