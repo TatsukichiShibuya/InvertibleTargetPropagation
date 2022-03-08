fcb29baa242a06d641c22c584865705a99fee47b
TatsukichiShibuya <tatsukichi78@gmail.com>
2022-03-08 14:23:12 +0900

    update

--- code/job_fa.sh
+++ code/job_fa.sh
@@ -9,6 +9,6 @@ export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
-if [ "${1}" = "cifar100" ];then
-  wandb agent tatsukichishibuya/InvTP/fnrv0asa
+if [ "${1}" = "fashion" ];then
+  wandb agent tatsukichishibuya/InvTP/u14ia1vt
 fi
