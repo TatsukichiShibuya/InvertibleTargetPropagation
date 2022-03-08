ca920421b232a61a58f3e54a4a6a439c45579bf4
TatsukichiShibuya <tatsukichi78@gmail.com>
2022-03-08 20:00:19 +0900

    update

--- code/wrapper_CFB.py
+++ code/wrapper_CFB.py
@@ -1,6 +1,6 @@
 import os
 import sys
 args = ' '.join(map(str, sys.argv[1:]))
-command = f'python Main.py --algorithm=InvTP {args} --label_augmentation --type=C --log --agent'
+command = f'python Main.py --algorithm=InvTP {args} --update_back_weights --label_augmentation --type=C --log --agent'
 print(command)
 os.system(command)
