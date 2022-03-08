bc7ecbcb3ce8a510cc5f1137accc489c8c2b05d4
TatsukichiShibuya <tatsukichi78@gmail.com>
2022-03-08 11:28:35 +0900

    update

--- code/job_RTP.sh
+++ code/job_RTP.sh
@@ -9,6 +9,6 @@ export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
-if [ "${1}" = "cifar100" ];then
-  wandb agent tatsukichishibuya/InvTP/1i0irelf
+if [ "${1}" = "fashion" ];then
+  wandb agent tatsukichishibuya/InvTP/fgb5595s
 fi
