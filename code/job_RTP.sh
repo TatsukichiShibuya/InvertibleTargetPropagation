484e1297a0b83da7111662a5a58addfbab487905
TatsukichiShibuya <tatsukichi78@gmail.com>
2022-03-10 11:30:04 +0900

    udpate

--- code/job_RTP.sh
+++ code/job_RTP.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$ -cwd
 #$ -l f_node=1
-#$ -l h_rt=03:30:00
+#$ -l h_rt=04:00:00
 #$ -j y
 #$ -o output/o.$JOB_ID
 export PYENV_ROOT="$HOME/.pyenv"
@@ -10,5 +10,5 @@ eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 if [ "${1}" = "fashion" ];then
-  wandb agent tatsukichishibuya/InvTP/fgb5595s
+  wandb agent tatsukichishibuya/InvTP/kvpgwq6f
 fi
